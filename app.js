33e1ba503b451e8f90886637796dd9e7c4d368ad
Med1z1n <99459061+Med1z1n@users.noreply.github.com>
2025-10-01 08:32:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -677,8 +677,8 @@ class PongGame {
         if (this.gameOver) return this.restartGame();
 
         if (value.startsWith("1:")) this.movePaddle(this.leftPaddle, -this.paddleSpeed);  // Left up
-        else if (value.startsWith("2:")) this.movePaddle(this.leftPaddle, this.paddleSpeed); // Left down
-        else if (value === "2") this.movePaddle(this.rightPaddle, -this.paddleSpeed); // Right up
+        else if (value === "3") this.movePaddle(this.leftPaddle, this.paddleSpeed); // Left down
+        else if (value.startsWith("2:")) this.movePaddle(this.rightPaddle, -this.paddleSpeed); // Right up
         else if (value === "4") this.movePaddle(this.rightPaddle, this.paddleSpeed); // Right down
     }
 
